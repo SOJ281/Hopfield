c7135daebb404301651335c7d4db6281a91d435d
poleap <pearce05@ntlworld.com>
2024-05-07 23:39:37 +0100

    Added code to plot effect of beta

--- plotResults.py
+++ plotResults.py
@@ -29,7 +29,7 @@ def plot_corruption(filename, title):
     plt.legend()
     plt.show()
 
-def plot_thetas(filename, title):
+def plot_thetas(filename, title, levels):
     file = open(filename,'r')
 
     pattern_nums = []
@@ -43,7 +43,7 @@ def plot_thetas(filename, title):
         CorruptionLevels.append(float(line[2]))
         thetas.append(float(line[4]))
 
-    for level in [0.0,0.1,0.2,0.4,0.8]:#set(CorruptionLevels):
+    for level in levels:#set(CorruptionLevels):
         level_pattern_nums = []
         level_ErrorRates = []
         for i in range(len(pattern_nums)):
@@ -54,6 +54,37 @@ def plot_thetas(filename, title):
             plt.plot(level_pattern_nums, level_ErrorRates, label="theta = %s" % (float(level)), linestyle='dashed')
         else:
             plt.plot(level_pattern_nums, level_ErrorRates, label="theta = %s" % (float(level)))
+    plt.title(title)
+    plt.xlabel("Number of Patterns")
+    plt.ylabel("Error Rate")
+    plt.legend()
+    plt.show()
+
+def plot_betas(filename, title, levels):
+    file = open(filename,'r')
+
+    pattern_nums = []
+    ErrorRates = []
+    CorruptionLevels = []
+    thetas = []
+    for line in file.readlines()[1:]:
+        line = line.strip().split(',')
+        pattern_nums.append(int(line[0]))
+        ErrorRates.append(float(line[1]))
+        CorruptionLevels.append(float(line[2]))
+        thetas.append(float(line[4]))
+
+    for level in levels:#set(CorruptionLevels):
+        level_pattern_nums = []
+        level_ErrorRates = []
+        for i in range(len(pattern_nums)):
+            if thetas[i] == level:
+                level_pattern_nums.append(pattern_nums[i])
+                level_ErrorRates.append(ErrorRates[i])
+        if level == 0.0:
+            plt.plot(level_pattern_nums, level_ErrorRates, label="beta = %s" % (float(level)), linestyle='dashed')
+        else:
+            plt.plot(level_pattern_nums, level_ErrorRates, label="beta = %s" % (float(level)))
     plt.title(title)
     plt.xlabel("Number of Patterns")
     plt.ylabel("Error Rate")
@@ -73,7 +104,13 @@ def append_columns(infile, outfile,new_collumns):
 
 if __name__ == '__main__':
     #append_columns("Results\HopfieldError.csv","Results\HopfieldError_withthetas.csv","100,0.0")
-    plot_thetas("Results\HopfieldErrorNoCorruptionThetas62136.csv", "Error Rate vs. value of theta (no corruption)")
-    plot_corruption("Results\HopfieldErrorCorruption3.csv", "Hopfield: Error Rate vs. % Corruption of Original Image")
-    plot_corruption("Results\HopfieldErrorDAM7139.csv", "DAM: Error Rate vs. % Corruption of Original Image")
-    plot_corruption("Results\HopfieldErrorContinuous71014.csv", "Continuous: Error Rate vs. % Corruption of Original Image")
+    
+    #plot_thetas("Results\HopfieldErrorNoCorruptionThetas62136.csv", "Error Rate vs. value of theta (no corruption)", [0.0,0.1,0.2,0.4,0.8])
+    #plot_corruption("Results\HopfieldErrorCorruption3.csv", "Hopfield: Error Rate vs. % Corruption of Original Image")
+    #plot_corruption("Results\HopfieldErrorDAM7139.csv", "DAM: Error Rate vs. % Corruption of Original Image")
+    # plot_corruption("Results\HopfieldErrorContinuous71014.csv", "Continuous: Error Rate vs. % Corruption of Original Image")
+    # plot_betas("Results\HopfieldErrorContinuousDifferentbetas71335.csv", "Error Rate vs. value of beta (no corruption)", [0.0,0.25,0.5,1.0,2.0,4.0])
+
+    plot_corruption("Results\HopfieldErrorContinuousBinary71912.csv", "Continuous: Error Rate vs. % Corruption of Original Image")
+    plot_betas("Results\HopfieldErrorContinuousDifferentbetas72020.csv", "Error Rate vs. value of beta (no corruption)", [0.0,0.25,0.5,1.0,2.0,4.0])
+    
